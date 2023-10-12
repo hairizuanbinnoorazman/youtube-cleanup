e7446af351dc3dcd02fec60bf1467d24514b544d
Hairizuan <hairizuanbinnoorazman@gmail.com>
2023-10-13 07:31:19 +0800

    move around line to log when youtube short video shelf deleted

--- scripts/content.js
+++ scripts/content.js
@@ -11,15 +11,18 @@ function listener()
         if (item.getAttribute('href') == null) { return; }
         if (item.href.includes('https://www.youtube.com/shorts')) {
             item.closest('ytd-video-renderer').remove();
-        } 
-        console.info('deleted youtube shorts video');
+            console.info('deleted youtube shorts video');
+        }
     });
 }
 
 var timeout = null;
+// DOMSubtreeModified is an event that triggers when any child node within it changes
 document.addEventListener("DOMSubtreeModified", function() {
     if(timeout) {
+        // Cancels the timeout previously set
         clearTimeout(timeout);
     }
+    // Run the following function once timer expires
     timeout = setTimeout(listener, 500);
 }, false);
